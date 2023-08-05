f1ef0af75cb7f9c4ef4c0498bc37f8200d15a493
SeAntipenko <antipenkos@mail.ru>
2023-08-05 15:16:57 +0300

    арифметические операторы

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-const a = 1;
-console.log(a);
-a = 'test';
-console.log(a);
+// базовые арифметичемике операторы
+
+const width = 10;
+const height = 5;
+const space = width * height;
+const newWidth = width - 4;
+const newWidth2 = width + 4;
+const division = newWidth / newWidth2;
+const volume = 2 ** 3;
+console.log(volume); 
+
+// строки 
+const city = 'Москва';
+const street = 'Новослободская';
+console.log (city + ', ' + street)
